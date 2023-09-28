7339d54d87a973f64a6b76ea2bb075b93e82105e
Prasuna <dahalprasuna2001@gmai.com>
2023-09-28 10:08:33 +0545

    adding uuid

--- modules/qr/qr.model.js
+++ modules/qr/qr.model.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const { v4: uuidv4 } = require("uuid");
 
 const qrSchema = new Schema({
   qrtext: { type: String },
+  qrId: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+  },
 });
 
 module.exports = mongoose.model("QrCode", qrSchema);
