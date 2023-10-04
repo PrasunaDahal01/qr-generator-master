2c64edc20b3c61099e9118f5d485a11c0a1e4304
Prasuna <dahalprasuna2001@gmai.com>
2023-10-04 16:18:25 +0545

    aggregates

--- modules/reports/report.controller.js
+++ modules/reports/report.controller.js
@@ -3,10 +3,44 @@ const qrModel = require("../qr/qr.model");
 const scanModel = require("../scan/scan.model");
 
 class Report {
-  //it uses the qrmodel to fetch all documents from the qrcodes and return them as an array.
   async getQrCodes() {
     try {
       const qrCodes = await qrModel.find();
+      const pipeline = [
+        {
+          $lookup: {
+            from: "qrscaninfos",
+            localField: "_id",
+            foreignField: "qrDocumentId",
+            as: "scanDetails",
+          },
+        },
+        {
+          $addFields: {
+            uniqueScans: {
+              $size: "$scanDetails",
+            },
+            totalCount: {
+              $sum: "$scanDetails.count",
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            qrId: 1,
+            qrText: 1,
+            scanDetails: 1,
+            uniqueScans: 1,
+            totalCount: 1,
+          },
+        },
+      ];
+      const result = await qrModel.aggregate(pipeline); //result is an array of documents that match the aggregation criteria.
+      if (result.length > 0) {
+        return result;
+      }
+
       return qrCodes;
     } catch (error) {
       throw error;
