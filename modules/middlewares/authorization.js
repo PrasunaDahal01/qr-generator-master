d1d8b4016d81df4d5deb325fbf4ec3bae612cde2
Karun Gyawali <karun.gyawali@rumsan.net>
2023-11-02 15:23:42 +0545

    restructuring the files

---
+++ modules/middlewares/authorization.js
@@ -0,0 +1,29 @@
+const userModel = require("../user/user.model");
+const jwt = require("jsonwebtoken");
+
+//authentication middleware to verify the jwt token.
+const userAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    console.log("token", token);
+
+    if (!token) {
+      return res.status(401).send("No Token Provided");
+    }
+
+    const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+    console.log("verifyUser", verifyUser);
+
+    const user = await userModel.findOne({ _id: verifyUser.id });
+    console.log("user", user);
+
+    req.token = token;
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).send(error);
+  }
+};
+
+module.exports = userAuth;
